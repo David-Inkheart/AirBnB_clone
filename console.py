b8e03d5adec5ab2221b29989d8203829d8279bc8
David-Inkheart <okoliedave1@gmail.com>
2022-11-24 07:26:23 -0800

    test console

--- console.py
+++ console.py
@@ -5,7 +5,10 @@ the entry point of the command interpreter
 '''
 
 
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 import cmd
+import models
 import sys
 
 
@@ -15,7 +18,7 @@ class HBNBCommand(cmd.Cmd):
     '''
     intro = None
     prompt = '(hbnb) '
-    file = None
+    file = 'file.json'
 
     ''' ------basic console commands----- '''
 
@@ -28,6 +31,39 @@ class HBNBCommand(cmd.Cmd):
         '''close the console, and exit:  QUIT'''
         return True
 
+    def do_create(self, arg):
+        '''Creates a new instance of BaseModel
+        saves it (to the JSON file) and prints the id
+        '''
+        if not arg:
+            print ('** class name missing **')
+        elif arg == 'BaseModel':
+            x = BaseModel()
+            x.save()
+            print(x.id)
+        elif arg[0] != 'BaseModel':
+            print ("** class doesn't exist **")
+
+    def do_show(self, arg):
+        '''Prints the string representation of an instance
+        based on the class name and id
+        '''
+        arg_list = arg.split(' ')
+        arg_list.append('x')
+        if arg_list[0] == '':
+           print('** class name missing **')
+        elif arg_list[0] != 'BaseModel':
+           print("** class doesn't exist **")
+        elif arg_list[1] == 'x':
+           print('** instance id missing **')
+        elif arg_list[1]:
+            storage = FileStorage().all()
+            for key in storage:
+                if key == 'BaseModel.' + arg_list[1]:
+                    print(BaseModel(storage[key]))
+                    return 
+            print('** no instance found **')
+                
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
